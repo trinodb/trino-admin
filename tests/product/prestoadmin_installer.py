e1cc453adb1dac814aa114f56dfd6f0374e11840
Karol Sobczak <karol.sobczak@karolsobczak.com>
2018-06-28 13:20:40 +0200

    Install explicit dependencies first

--- tests/product/prestoadmin_installer.py
+++ tests/product/prestoadmin_installer.py
@@ -113,16 +113,12 @@ class PrestoadminInstaller(BaseInstaller):
             installer_container.run_script_on_host(
                 'set -e\n'
                 # use explicit versions of dependent packages
-                'pip install --upgrade pycparser==2.18 cffi==1.11.5\n'
-                'pip install --upgrade pycparser==2.18 PyNaCl==1.2.1\n'
-                'pip install --upgrade pycparser==2.18 cryptography==2.1.1\n'
-                'pip install --upgrade pip==7.1.2\n'
-                'pip install --upgrade wheel==0.23.0\n'
-                'pip install --upgrade setuptools==20.1.1\n'
-                # use explicit versions of dependent packages
                 'pip install --upgrade cffi==1.11.5\n'
                 'pip install --upgrade PyNaCl==1.2.1\n'
                 'pip install --upgrade cryptography==2.1.1\n'
+                'pip install --upgrade pip==7.1.2\n'
+                'pip install --upgrade wheel==0.23.0\n'
+                'pip install --upgrade setuptools==20.1.1\n'
                 'mv %s/presto-admin ~/\n'
                 'cd ~/presto-admin\n'
                 'make %s\n'
