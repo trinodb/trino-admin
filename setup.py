79178ecdbc1ddd81572952eadab22bdf4990610e
Anton Petrov <petroav@users.noreply.github.com>
2019-10-17 10:36:06 +0100

    Bump paramiko version to include new ciphers

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with open('README.md') as readme_file:
 requirements = [
     'pycparser==2.18',
     'argparse==1.4',
-    'paramiko==1.15.3',
+    'paramiko==1.16.0',
     'Fabric==1.10.3',
     'requests==2.7.0',
     'overrides==0.5',
